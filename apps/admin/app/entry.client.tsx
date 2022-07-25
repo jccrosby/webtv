7bc839ea095432aaa94fcfde965c592639abd534
jcrosby <john.crosby@mlb.com>
2022-07-25 13:58:31 -0500

    chore: fix hydrate

--- apps/admin/app/entry.client.tsx
+++ apps/admin/app/entry.client.tsx
@@ -1,10 +1,10 @@
 import { RemixBrowser } from '@remix-run/react';
-import { hydrate } from 'react-dom';
+import { hydrateRoot } from 'react-dom/client';
 import { ClientProvider } from '@mantine/remix';
 
-hydrate(
-  <ClientProvider>
-    <RemixBrowser />
-  </ClientProvider>,
-  document
+hydrateRoot(
+    document,
+    <ClientProvider>
+        <RemixBrowser />
+    </ClientProvider>,
 );
